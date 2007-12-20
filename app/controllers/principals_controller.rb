b686f76122a72d83cd855ba36c1b2cb2a69221ef
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2007-12-20 00:23:01 +0000

    finished CRUD use cases for principal/principal_type
    git-svn-id: svn+ssh://www.preallowed.com/apps/svn.preallowed/trunk/preallowed@21 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- app/controllers/principals_controller.rb
+++ app/controllers/principals_controller.rb
@@ -1,17 +1,57 @@
 class PrincipalsController < ApplicationController
+  before_filter :find_subject
 
   def new
+    @principal = Principal.new
   end
 
   def edit
+    @principal = @subject.principals.find(params[:id])
   end
 
   def create
+    @principal = Principal.new(params[:principal])
+    if (@subject.principals << @principal)
+      redirect_to client_subject_url(@client, @subject)
+    else 
+      render :action => :new
+    end
   end
 
   def update
+    @principal = @subject.principals.find(params[:id])
+    if @principal.update_attributes(params[:principal])
+      redirect_to client_subject_url(@client, @subject)
+    else
+      render :action => :edit
+    end
   end
 
   def destroy
+    @principal = @subject.principals.find(params[:id])
+    @subject.principals.destroy(@principal)
   end
+
+  def show
+    @principal = @subject.principals.find(params[:id])
+    respond_to do |format|
+      format.html # show.rhtml
+      format.xml {render :xml => @principal.to_xml }
+    end
+  end
+
+
+  private
+  
+  def find_subject
+    @client_id = params[:client_id]
+    @subject_id = params[:subject_id]
+
+    redirect_to clients_url unless @client_id
+    @client = Client.find(@client_id)
+        
+    redirect_to client_subjects_url(@client.id) unless @subject_id
+    @subject = @client.subjects.find(@subject_id)
+  end
+
 end
