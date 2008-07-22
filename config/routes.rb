32662c4741b0e18a60928c06c679e2ca87c4c3e3
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2008-07-22 18:46:11 +0000

    not a change
    git-svn-id: svn+ssh://www.preallowed.com/apps/svn.preallowed/trunk/preallowed@66 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
   # map.connect '', :controller => "welcome"
-  map.home '',  :controller => 'home', 
-                :action => "index"
+  map.home '/',  :controller => "home", :action => "index"
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
 #  map.connect ':controller/service.wsdl', :action => 'wsdl'
