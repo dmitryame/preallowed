1b774890fc13e34592aa5f69fb73e9abfe4dccec
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2008-08-10 22:06:06 +0000

    added comment
    git-svn-id: svn+ssh://www.preallowed.com/apps/svn.preallowed/trunk/preallowed@90 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -96,6 +96,7 @@ class ApplicationController < ActionController::Base
     end 
   end 
 
+# defines resources that never require the authentication
   def skip_authentication
     return true if request.path == '/home/insufficient'
     return true if request.path == '/clients/new'  # we need the next two lines as an exception, so that the users can self register new clients.
