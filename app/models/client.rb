33b57db1f1df9d0920eac19ac006edf5b9842fb9
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2008-08-13 20:58:24 +0000

    making sure the first client is preallowed automatically
    git-svn-id: svn+ssh://www.preallowed.com/apps/svn.preallowed/trunk/preallowed@120 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,4 +7,7 @@ class Client < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_uniqueness_of :preallowed, :if => :preallowed? #make sure there is only one instance of client preallowed
 
+  def before_create 
+      self.preallowed = true if (Client.count(:conditions => ["preallowed = true"]) == 0) #the very first client will be preallowed
+  end
 end
