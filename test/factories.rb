2d3dacd06e38ebe91655cadefbe5c752f3ab77cf
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-07-15 20:26:29 +0200

    Remove fixtures

--- test/factories.rb
+++ test/factories.rb
@@ -15,25 +15,25 @@ Factory.sequence :value do |n|
 end
 
 Factory.define :client do |client|
-  client.name {Factory.next :name }
-  client.email {Factory.next :email } 
+  client.name { Factory.next :name }
+  client.email { Factory.next :email } 
 end
 
 Factory.define :subject do |subject|
-  subject.name {Factory.next :name }
-  subject.email {Factory.next :email }
-  subject.password {Factory.next :password }
-  subject.password_confirmation {|s| s.password }
+  subject.name { Factory.next :name }
+  subject.email { Factory.next :email }
+  subject.password { Factory.next :password }
+  subject.password_confirmation { |s| s.password }
   subject.association :client  
 end
 
 Factory.define :role do |role|
-  role.name {Factory.next :name }
+  role.name { Factory.next :name }
   role.association :client  
 end
 
 Factory.define :resource do |resource|
-  resource.name {Factory.next :name }
+  resource.name { Factory.next :name }
   resource.association :client  
 end
 
@@ -48,17 +48,17 @@ Factory.define :resources_association do |resources_association|
 end
 
 Factory.define :profile do |profile|
-  profile.managed_client {|managed_client| managed_client.association(:client) }
-  profile.administrator {|administrator| administrator.association(:subject) }
+  profile.managed_client { |managed_client| managed_client.association(:client) }
+  profile.administrator { |administrator| administrator.association(:subject) }
 end
 
 Factory.define :access_log do |acceess_log|
   acceess_log.association :client  
-  acceess_log.request_body     {Factory.next :value }
-  acceess_log.request_headers  {Factory.next :value }
-  acceess_log.request_method   {Factory.next :value }
-  acceess_log.request_path     {Factory.next :value }
-  acceess_log.response_body    {Factory.next :value }
-  acceess_log.response_headers {Factory.next :value }
-  acceess_log.response_status  {Factory.next :value }
+  acceess_log.request_body     { Factory.next :value }
+  acceess_log.request_headers  { Factory.next :value }
+  acceess_log.request_method   { Factory.next :value }
+  acceess_log.request_path     { Factory.next :value }
+  acceess_log.response_body    { Factory.next :value }
+  acceess_log.response_headers { Factory.next :value }
+  acceess_log.response_status  { Factory.next :value }
 end
