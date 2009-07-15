306187334953676495d9e52972e956ed04689f96
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-07-15 17:06:18 +0200

    Remove deprecated option for protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => '1f648df8c66dfhkjhakjhdkh90534o5jokj345kjoklec4fa37661768b2d93b'
+  protect_from_forgery
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
